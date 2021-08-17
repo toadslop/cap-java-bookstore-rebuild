5e2644d5d895142ff2de0818c4c60e349f5873a2
brian heise <bnheise@gmail.com>
2021-08-18 07:37:26 +0900

    Part 8: Step 2 -- The 'On' Lifecycle Method
    Changes to be committed:
    	modified:   srv/src/main/java/toadslop/bookshop/handlers/CatalogServiceHandler.java

--- srv/src/main/java/toadslop/bookshop/handlers/CatalogServiceHandler.java
+++ srv/src/main/java/toadslop/bookshop/handlers/CatalogServiceHandler.java
@@ -2,13 +2,20 @@ package toadslop.bookshop.handlers;
 
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.handler.annotations.On;
 
 import org.springframework.stereotype.Component;
 
 import cds.gen.catalogservice.CatalogService_;
+import cds.gen.catalogservice.AddReviewContext;
 
 @Component
 @ServiceName(CatalogService_.CDS_NAME)
 public class CatalogServiceHandler implements EventHandler {
-    
+
+    @On(event = AddReviewContext.CDS_NAME)
+    public void addReview() {
+        System.out.println("Triggered the action!");
+    }
+
 }
