61e41fc17c58bf617ecc8a338949860b312868df
brian heise <bnheise@gmail.com>
2021-09-01 06:13:04 +0900

    Part 10: Step 2 -- Extracting the ID from the CQN Using the CqnAnalyzer
    Changes to be committed:
    	modified:   srv/src/main/java/toadslop/bookshop/handlers/CatalogServiceHandler.java

--- srv/src/main/java/toadslop/bookshop/handlers/CatalogServiceHandler.java
+++ srv/src/main/java/toadslop/bookshop/handlers/CatalogServiceHandler.java
@@ -1,6 +1,10 @@
 package toadslop.bookshop.handlers;
 
+import java.util.Map;
+
+import com.sap.cds.ql.cqn.AnalysisResult;
 import com.sap.cds.ql.cqn.CqnAnalyzer;
+import com.sap.cds.ql.cqn.CqnSelect;
 import com.sap.cds.reflect.CdsModel;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.ServiceName;
@@ -11,6 +15,7 @@ import org.springframework.stereotype.Component;
 import cds.gen.catalogservice.CatalogService_;
 import cds.gen.catalogservice.Reviews;
 import cds.gen.catalogservice.AddReviewContext;
+import cds.gen.catalogservice.Books;
 
 @Component
 @ServiceName(CatalogService_.CDS_NAME)
@@ -20,13 +25,16 @@ public class CatalogServiceHandler implements EventHandler {
 
     CatalogServiceHandler(CdsModel model) {
         this.analyzer = CqnAnalyzer.create(model);
-	}
+    }
 
     @On(event = AddReviewContext.CDS_NAME)
     public void addReview(AddReviewContext context) {
-        System.out.println(context.getCqn());
+        CqnSelect select = context.getCqn();
+        String bookId = (String) analyzer.analyze(select).targetKeys().get(Books.ID);
+        System.out.println(bookId);
 
         Reviews review = Reviews.create();
+        review.setBookId(bookId);
         review.setText(context.getTitle());
         review.setRating(context.getRating());
         review.setText(context.getText());
