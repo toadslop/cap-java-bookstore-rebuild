a766d9f4f00fb410931bf102f2a7141d56eb72bd
brian heise <bnheise@gmail.com>
2021-09-01 07:08:29 +0900

    Part 10: Step 3 -- Saving the Review to the Database
    Changes to be committed:
    	modified:   srv/src/main/java/toadslop/bookshop/handlers/CatalogServiceHandler.java

--- srv/src/main/java/toadslop/bookshop/handlers/CatalogServiceHandler.java
+++ srv/src/main/java/toadslop/bookshop/handlers/CatalogServiceHandler.java
@@ -1,19 +1,20 @@
 package toadslop.bookshop.handlers;
 
-import java.util.Map;
-
-import com.sap.cds.ql.cqn.AnalysisResult;
+import com.sap.cds.ql.Insert;
 import com.sap.cds.ql.cqn.CqnAnalyzer;
+import com.sap.cds.ql.cqn.CqnInsert;
 import com.sap.cds.ql.cqn.CqnSelect;
 import com.sap.cds.reflect.CdsModel;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.persistence.PersistenceService;
 import com.sap.cds.services.handler.annotations.On;
 
 import org.springframework.stereotype.Component;
 
 import cds.gen.catalogservice.CatalogService_;
 import cds.gen.catalogservice.Reviews;
+import cds.gen.catalogservice.Reviews_;
 import cds.gen.catalogservice.AddReviewContext;
 import cds.gen.catalogservice.Books;
 
@@ -21,9 +22,11 @@ import cds.gen.catalogservice.Books;
 @ServiceName(CatalogService_.CDS_NAME)
 public class CatalogServiceHandler implements EventHandler {
 
+    private final PersistenceService db;
     private final CqnAnalyzer analyzer;
 
-    CatalogServiceHandler(CdsModel model) {
+    CatalogServiceHandler(PersistenceService db, CdsModel model) {
+        this.db = db;
         this.analyzer = CqnAnalyzer.create(model);
     }
 
@@ -31,13 +34,15 @@ public class CatalogServiceHandler implements EventHandler {
     public void addReview(AddReviewContext context) {
         CqnSelect select = context.getCqn();
         String bookId = (String) analyzer.analyze(select).targetKeys().get(Books.ID);
-        System.out.println(bookId);
 
         Reviews review = Reviews.create();
         review.setBookId(bookId);
         review.setText(context.getTitle());
         review.setRating(context.getRating());
         review.setText(context.getText());
+
+        CqnInsert reviewInsert = Insert.into(Reviews_.CDS_NAME).entry(review);
+        db.run(reviewInsert);
 
         context.setCompleted();
     }
