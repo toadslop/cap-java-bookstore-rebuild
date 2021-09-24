332a88b218f36da0a0451b45b69dc10bf00b8e80
brian heise <bnheise@gmail.com>
2021-09-24 10:43:02 +0900

    Part 12: Step 2 -- Setting Up the Add Review Dialog
    Changes to be committed:
    	modified:   app/browse/webapp/custom/AddReview/AddReviewButton.fragment.xml
    	modified:   app/browse/webapp/custom/AddReview/AddReviewButtonHandler.js

--- app/browse/webapp/custom/AddReview/AddReviewButtonHandler.js
+++ app/browse/webapp/custom/AddReview/AddReviewButtonHandler.js
@@ -1,13 +1,19 @@
-sap.ui.define(["sap/m/MessageBox"], function (MessageBox) {
+sap.ui.define(["sap/ui/core/Fragment"], function (Fragment) {
   "use strict";
 
   return {
-    onInit: function () {
-      console.log("INIT!");
-    },
+    openDialog: async function () {
+      const oBooklistPage = sap.ui.getCore().byId("bookshop::BooksList");
+      console.log("OPEN DIALOG");
+      if (!this.oAddReviewDialog) {
+        this.oAddReviewDialog = await Fragment.load({
+          id: `${oBooklistPage.getId()}-AddReviewDialog`,
+          name: "bookshop.custom.AddReview.AddReviewDialog",
+        });
+        oBooklistPage.addDependent(this.oAddReviewDialog);
+      }
 
-    onPress: function () {
-      MessageBox.show("Button pressed!");
+      this.oAddReviewDialog.open();
     },
   };
 });
