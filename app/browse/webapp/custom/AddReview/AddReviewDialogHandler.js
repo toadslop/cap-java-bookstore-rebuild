820e4b6dcc7c0031f3e36be807f3a83ad7831dde
brian heise <bnheise@gmail.com>
2021-09-25 08:19:57 +0900

    Part 14: Step 2 -- Submit Logic
    Changes to be committed:
    	modified:   app/browse/webapp/custom/AddReview/AddReviewDialogHandler.js

--- app/browse/webapp/custom/AddReview/AddReviewDialogHandler.js
+++ app/browse/webapp/custom/AddReview/AddReviewDialogHandler.js
@@ -1,29 +1,17 @@
 sap.ui.define(
-  [
-    "sap/ui/core/Fragment",
-    "sap/ui/layout/form/FormContainer",
-    "sap/ui/layout/form/FormElement",
-    "sap/m/Input",
-  ],
-  function (Fragment, FormContainer, FormElement, Input) {
+  ["sap/ui/core/Fragment", "./createAddReviewFormContainer"],
+  function (Fragment, createAddReviewFormContainer) {
     "use strict";
 
     const getAddReviewDialog = (oEvent) => oEvent.getSource().getParent();
-
-    const oContainerTemplate = new FormContainer();
-    const oTitleElement = new FormElement({ label: "Title" });
-    const oTitleInput = new Input({ value: "{title}" });
-    oTitleElement.addField(oTitleInput);
-    oContainerTemplate.addFormElement(oTitleElement);
 
     return {
       beforeOpenDialog: function (oEvent, oParams) {
         const { sRowBindingPath, sReviewDialogId } = oParams;
         const oAddReviewForm = Fragment.byId(sReviewDialogId, "addReviewForm");
-
         oAddReviewForm.bindAggregation("formContainers", {
           path: `${sRowBindingPath}/reviews`,
-          template: oContainerTemplate,
+          template: createAddReviewFormContainer(),
           length: 1,
           parameters: {
             $$updateGroupId: "reviews",
@@ -38,8 +26,19 @@ sap.ui.define(
         });
       },
 
-      submit: function (oEvent) {
-        getAddReviewDialog(oEvent).close();
+      submit: async function (oEvent) {
+        const oAddReviewDialog = getAddReviewDialog(oEvent);
+        oAddReviewDialog.setBusy(true);
+
+        try {
+          await oAddReviewDialog.getModel().submitBatch("reviews");
+          console.log("SUCCESS");
+          oAddReviewDialog.close();
+        } catch (error) {
+          console.log(`ERROR: ${error.message}`);
+        } finally {
+          oAddReviewDialog.setBusy(false);
+        }
       },
 
       cancel: function (oEvent) {
